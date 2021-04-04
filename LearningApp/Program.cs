5cd146026d6086bb210a9149ad193920e1de0b4b
Ankush Parashar <parashar.anku@live.com>
2021-04-04 15:51:42 +0530

    Commit changes

---
+++ LearningApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LearningApp
+{
+    class Program
+    {
+        class test
+        {
+            public int a;
+            public int b; 
+            public void print()
+            {
+                Console.WriteLine("Hi");
+                string v = a.ToString();
+            }
+        }
+        static void Main(string[] args)
+        {
+            string myName = "ANKUSH";
+            string nyName2 = "ANKUSH";
+            string a = "MyName is ankush Parashar675853467";
+            string[] names = new [] { "ABHISHEK", "ANJU", "ACHHAR" };
+            List<string> abc = new List<string>();
+            List<test> tests = new List<test>();
+            myName.ToLower(); 
+            test t = tests.Where(x => x.a == 2).FirstOrDefault();
+
+            //CREATING AND ARRAY
+            
+            var entredString = Console.ReadLine();
+            entredString.Split(' ');
+            //Console.WriteLine(myName.CompareTo(nyName2));
+            //Console.WriteLine(a.Replace("67", "").ToString());
+        }
+    }
+}
